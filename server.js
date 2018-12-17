94abee6cf3033c3ce5b11d1b82ab635aa4f9f3ab
wparam <wparam@hotmail.com>
2018-12-17 12:36:47 -0500

    update some service

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const http = require('http');
-
 const ports = [20000, 20001];
 
 const startServer = port => {
@@ -12,6 +11,12 @@ const startServer = port => {
             return res.end();
         }
         console.log(`${new Date()}, Get incoming request to ${servername}, host is ${req.headers.host}, url is ${req.url}`);
+        if(/\/data\/check/g.test(req.url)){
+            return res.end('success');
+        }
+        if(/\/data\/mockapi/g.test(req.url)){
+            return res.end('data service is succesfully returned');
+        }
         res.end('ok');
     }).once('error', (err)=>{
         if(err.code === 'EADDRINUSE'){
